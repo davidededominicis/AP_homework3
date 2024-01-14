6635f510637f6d7f2d8f51275e092a5708e5b212
3sara <84321374+3sara@users.noreply.github.com>
2024-01-14 12:02:47 +0100

    Update ode_bind.cpp

--- src/ode_bind.cpp
+++ src/ode_bind.cpp
@@ -1,23 +1,32 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
-#include "odesolver.hpp"
 #include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <pybind11/chrono.h>
+#include "odesolver.hpp"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(odesolver, m) {
-	m.doc() = "Python bind of CSVParser class";
+    m.doc() = "Python bind of CSVParser class";
 
-	py::class_<ODESolver>(m, "ODESolver")
-	.def(py::init<const std::function<Eigen::VectorXd(double, Eigen::VectorXd)> &, double, double, Eigen::VectorXd &>())
-		.def("RK4", &ODESolver::RK4)
-		.def("RK4_csv", &ODESolver::RK4_csv)
-		.def("midpoint", &ODESolver::midpoint)
-		.def("midpoint_csv", &ODESolver::midpoint_csv)
-		.def("euler", &ODESolver::euler)
-		.def("euler_csv", &ODESolver::euler_csv)
-		.def("accuracy", &ODESolver::accuracy)
-		.def("efficiency", &ODESolver::efficiency)
-		.def("stability", &ODESolver::stability)
-		.def("convergence", &ODESolver::convergence);
+    // Dichiarazione della classe ODESolution
+    py::class_<ODESolution>(m, "ODESolution")
+        .def_readwrite("t", &ODESolution::t)
+        .def_readwrite("Y", &ODESolution::Y);
+
+    py::class_<ODESolver>(m, "ODESolver")
+        .def(py::init([](function<VectorXd(double, VectorXd)> fun, double t_start, double t_end, VectorXd y0) {
+            return new ODESolver(fun, t_start, t_end, y0);
+        }))
+        .def("RK4", &ODESolver::RK4)
+        .def("RK4_csv", &ODESolver::RK4_csv)
+        .def("midpoint", &ODESolver::midpoint)
+        .def("midpoint_csv", &ODESolver::midpoint_csv)
+        .def("euler", &ODESolver::euler)
+        .def("euler_csv", &ODESolver::euler_csv)
+        .def("accuracy", &ODESolver::accuracy)
+        .def("efficiency", &ODESolver::efficiency)
+        .def("stability", &ODESolver::stability)
+        .def("convergence", &ODESolver::convergence);
 }
